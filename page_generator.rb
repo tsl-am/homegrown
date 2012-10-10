734465d5d3b19898bee36969bf57641508df7dd3
Jeff Cohen <jeff@codeacademy.org>
2012-10-10 11:29:07 -0500

    Define our own class to generate web pages

--- page_generator.rb
+++ page_generator.rb
@@ -1,31 +1,83 @@
 require 'open-uri'
 require 'json'
 
-json_response = open("http://codemountain.com/api/students/am.json").read
+class WebPageGenerator
 
-students = JSON.parse(json_response)
+  # Create a method that will generate a web page containing
+  # just one student selected at random.
 
-puts "<html>"
-puts "	<head>"
-puts "		<title>TSL AM Dev Students</title>"
-puts "		<link rel='stylesheet' href='reset.css' type='text/css'>"
-puts "		<link rel='stylesheet' href='styles.css' type='text/css'>"
-puts "		<link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>"
-puts "	</head>"
-puts "	<body>"
-puts "		<h1>"
-puts "			TSL AM Dev Students"
-puts "		</h1>"
-puts "		<ul>"
+  def get_students
+    json_response = open("http://codemountain.com/api/students/am.json").read
+    return JSON.parse(json_response)
+  end
 
-students.each do |student_hash|
-  puts "      <li class='frame'>"
-  puts "    	  <img class='thumbnail' src='http://codemountain.com#{student_hash["student"]["avatar_url"]}'>"
-  puts "    	  <p class='name'>#{student_hash["student"]["name"]}</p>"
-  puts "    	</li>"
+  def html_header
+    puts "<html>"
+    puts "	<head>"
+    puts "		<title>TSL AM Dev Students</title>"
+    puts "		<link rel='stylesheet' href='reset.css' type='text/css'>"
+    puts "		<link rel='stylesheet' href='styles.css' type='text/css'>"
+    puts "		<link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>"
+    puts "	</head>"
+    puts "	<body>"
+    puts "		<h1>"
+    puts "			TSL AM Dev Students"
+    puts "		</h1>"
+    puts "		<ul>"
+  end
+  
+  def generate_random_student
+    students = get_students
+
+    html_header
+    
+    random_student = students.sample
+
+    puts "      <li class='frame'>"
+    puts "    	  <img class='thumbnail' src='http://codemountain.com#{random_student["student"]["avatar_url"]}'>"
+    puts "    	  <p class='name'>#{random_student["student"]["name"]}</p>"
+    puts "    	</li>"
+
+    html_footer
+    
+  end
+
+  def html_footer
+    puts "    </ul>"
+    puts "	</body>"
+    puts "</html>"
+  end
+  
+  def generate_all_students
+    json_response = open("http://codemountain.com/api/students/am.json").read
+
+    students = JSON.parse(json_response)
+
+    puts "<html>"
+    puts "	<head>"
+    puts "		<title>TSL AM Dev Students</title>"
+    puts "		<link rel='stylesheet' href='reset.css' type='text/css'>"
+    puts "		<link rel='stylesheet' href='styles.css' type='text/css'>"
+    puts "		<link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>"
+    puts "	</head>"
+    puts "	<body>"
+    puts "		<h1>"
+    puts "			TSL AM Dev Students"
+    puts "		</h1>"
+    puts "		<ul>"
+
+    students.each do |student_hash|
+      puts "      <li class='frame'>"
+      puts "    	  <img class='thumbnail' src='http://codemountain.com#{student_hash["student"]["avatar_url"]}'>"
+      puts "    	  <p class='name'>#{student_hash["student"]["name"]}</p>"
+      puts "    	</li>"
+    end
+
+    puts "    </ul>"
+    puts "	</body>"
+    puts "</html>"
+  end
+
 end
 
-puts "    </ul>"
-puts "	</body>"
-puts "</html>"
 
--- router.rb
+++ router.rb
@@ -1,18 +1,17 @@
+require_relative 'page_generator'
+
 if ARGV.size == 0
   puts "Usage: ruby router.rb [action]"
-  exit
-end
-
-action = ARGV[0]
-
-if action == 'index'
-  # 
-  # What code goes here?
-  #
-elsif action == 'random'
-  #
-  # What code goes here?
-  #
 else
-  puts "Unknown action!  Try again."
+  action = ARGV[0]
+  generator = WebPageGenerator.new
+
+  if action == 'index'
+    generator.generate_all_students
+  elsif action == 'random'
+    generator.generate_random_student
+  else
+    puts "Unknown action!  Try again."
+  end
+  
 end
